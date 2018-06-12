33d0781decba19bf89134c29f81f13c9445ab288
lenka123123 <13902276741@163.com>
2018-06-12 13:35:51 +0800

    提交可以运行

---
+++ baselib/src/main/java/com/example/baselib/net/MainAativity.java
@@ -0,0 +1,19 @@
+package com.example.baselib.net;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * 创建日期：2018/6/12 on 13:27
+ * 描述:
+ * 作者:yuxd Administrator
+ */
+public class MainAativity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
